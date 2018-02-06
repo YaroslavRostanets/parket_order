82c4eb285e8f5bf7cf91dcb0b04a08d11f8f4ac9
Yaroslav R <rostanets@gmail.com>
2018-02-07 01:02:10 +0200

    fix

--- js/new_main.js
+++ js/new_main.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
-
+    $(".order-tabs .one-tab-btn").on("click", function(){
+        var activeTab = $($(this).attr("data-tab"));
+        $(".one-tab-btn").removeClass("active");
+        $(this).addClass("active");
+        activeTab.siblings().hide();
+        activeTab.show();
+    });
+    $(".order-tabs .one-tab-btn.active").click();
 });
+console.log("test");
